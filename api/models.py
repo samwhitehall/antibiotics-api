6e4e254913b5e634110ff1ba8bca3906ccbdc864
Sam Whitehall <me@samwhitehall.com>
2013-11-09 13:51:27 +0000

    Straightforward models defined

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Provider(models.Model):
+    slug = models.SlugField()
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Category(models.Model):
+    slug = models.SlugField()
+    name = models.CharField(max_length=100)
+
+class Diagnosis(models.Model):
+    slug = models.SlugField()
+    name = models.CharField(max_length=100)
