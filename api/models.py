82e406ac09165080c72ca24992f8b4d526a5b865
Sam Whitehall <me@samwhitehall.com>
2013-11-09 15:24:05 +0000

    Plural strings & verbose names for fields

--- api/models.py
+++ api/models.py
@@ -9,6 +9,9 @@ class Category(models.Model):
     slug = models.SlugField()
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name_plural = "categories"
+
 class Diagnosis(models.Model):
     slug = models.SlugField()
     name = models.CharField(max_length=100)
@@ -16,9 +19,15 @@ class Diagnosis(models.Model):
     provider = models.ForeignKey(Provider)
     category = models.ForeignKey(Category)
 
+    class Meta:
+        verbose_name_plural = "diagnoses"
+
 class DecisionTree(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     version_number = models.PositiveIntegerField(unique=True)
     published = models.BooleanField(default=False)
 
     diagnosis = models.ForeignKey(Diagnosis)
+
+    class Meta:
+        verbose_name = "decision tree"
