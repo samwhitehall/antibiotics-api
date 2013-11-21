75171fef64ee37d4982a075023fc0ba5ead2bfe6
Sam Whitehall <me@samwhitehall.com>
2013-11-21 21:28:34 +0000

    urlconf tweaks: url schemes default to live even if /live is not specified

--- antibiotics_data/urls.py
+++ antibiotics_data/urls.py
@@ -10,28 +10,29 @@ from django.contrib import admin
 admin.autodiscover()
 
 provider_urls = patterns('',
-    url(r'^live/', LiveProviderList.as_view(), name='live-provider-list'),
-    url(r'^test/', TestProviderList.as_view(), name='test-provider-list')
+    url(r'^test', TestProviderList.as_view(), name='test-provider-list'),
+    url(r'(?:live)?$', LiveProviderList.as_view(), name='live-provider-list')
+)
+
+tree_list_urls = patterns('',
+    url(r'^test', TestTreeList.as_view(), name='test-tree-list'),
+    url(r'^(?:live)?$', LiveTreeList.as_view(), name='live-tree-list'),
 )
 
 tree_urls = patterns('',
-    url(r'^/live', LiveIndividualTree.as_view(), name='live-individual-tree'),
-    url(r'^/test', TestIndividualTree.as_view(), name='test-individual-tree'),
+    url(r'^test', TestIndividualTree.as_view(), name='test-individual-tree'),
+    url(r'^(?:live)?$', LiveIndividualTree.as_view(), name='live-individual-tree'),
 )
 
 diagnosis_urls = patterns('',
-    url(r'^(?P<provider>[a-z]+)/live$', 
-        LiveTreeList.as_view(), name='live-tree-list'),
-    url(r'^(?P<provider>[a-z]+)/test$', 
-        TestTreeList.as_view(), name='test-tree-list'),
     url(r'^tree/(?P<pk>[0-9]+)/', 
         SpecificIndividualTree.as_view(), name='specific-individual-tree'),
     url(r'^tree/' \
             '(?P<provider>[a-z-]+)/' \
             '(?P<category>[a-z-]+)/' \
-            '(?P<diagnosis>[a-z-]+)', include(tree_urls))
+            '(?P<diagnosis>[a-z-]+)/', include(tree_urls)),
+    url(r'^(?P<provider>[a-z]+)/', include(tree_list_urls))
 )
-
 
 urlpatterns = patterns('',
     url(r'^providers/', include(provider_urls)),
