bad15c3be31d8b2b2d370f943580f332c5caf664
Sam Whitehall <me@samwhitehall.com>
2013-11-21 22:16:50 +0000

    basic refactoring

--- api/tree_rest.py
+++ api/tree_rest.py
@@ -6,32 +6,41 @@ from api.tree_serializers import IndividualTreeSerializer
 from api.models import DecisionTree
 
 class BaseIndividualTree(APIView):
-    def get_object(self, provider, category, diagnosis):
+    def get_trees(self, provider, category, diagnosis):
+        query = DecisionTree.objects.filter(
+            provider__slug=provider,
+            diagnosis__category__slug=category,
+            diagnosis__slug=diagnosis,
+        )
+
+        if self.Meta.published:
+            query = query.filter(published=True)
+
+        return query
+
+    def get(self, request, provider, category, diagnosis):
+        matching_trees = self.get_trees(provider, category, diagnosis)
+        tree = self.single_tree(matching_trees)
+
+        serializer = IndividualTreeSerializer(tree)
+        return Response(serializer.data)
+
+class BaseLatestIndividualTree(BaseIndividualTree):
+    def single_tree(self, query):
         try:
-            query = DecisionTree.objects.filter(
-                provider__slug=provider,
-                diagnosis__category__slug=category,
-                diagnosis__slug=diagnosis,
-            )
-
-            if self.Meta.published:
-                query = query.filter(published=True)
-
-            return query.order_by('published')[0:1]
-
+            # TODO: work out why this is sometimes None
+            if query:
+                return query.order_by('published')[0:1]
         except DecisionTree.DoesNotExist:
             raise Http404
 
-    def get(self, request, provider, category, diagnosis, format=None):
-        tree = self.get_object(provider, category, diagnosis)
-        serializer = IndividualTreeSerializer(tree)
-
-        return Response(serializer.data)
-
-class LiveIndividualTree(BaseIndividualTree):
+class LiveIndividualTree(BaseLatestIndividualTree):
     class Meta:
         published = True
 
-class TestIndividualTree(BaseIndividualTree):
+class TestIndividualTree(BaseLatestIndividualTree):
     class Meta:
         published = False
+
+class SpecificIndividualTree:
+    pass
