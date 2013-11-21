462a1594655218853b8225259efa1bfbcbec2d55
Sam Whitehall <me@samwhitehall.com>
2013-11-21 20:16:24 +0000

    dummy path field added to directory listing

--- api/diagnosis_serializers.py
+++ api/diagnosis_serializers.py
@@ -1,11 +1,17 @@
-from api.models import DecisionTree
 from rest_framework import serializers
+from rest_framework.reverse import reverse
+
+from api.models import DecisionTree
 
 class TestTreeSerializer(serializers.ModelSerializer):
     category = serializers.Field(source='diagnosis.category.name')
     diagnosis = serializers.Field(source='diagnosis.name')
     version = serializers.Field(source='version')
+    path = serializers.SerializerMethodField('get_tree_path')
+
+    def get_tree_path(self, tree):
+       return 'tree' 
 
     class Meta:
         model = DecisionTree
-        fields = ('category', 'diagnosis', 'version', )
+        fields = ('path', 'category', 'diagnosis', 'version' )
