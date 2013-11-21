ffe55328e2c3efd6dd88f830560b787c29f258bb
Sam Whitehall <me@samwhitehall.com>
2013-11-21 16:24:45 +0000

    renamed tree pys to diagnosis pys in front of splitting individual into new file

--- antibiotics_data/urls.py
+++ antibiotics_data/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from rest_framework import routers
 
 from api.provider_rest import LiveProviderList, TestProviderList
-from api.tree_rest import LiveTreeList, TestTreeList, IndividualTree
+from api.diagnosis_rest import LiveTreeList, TestTreeList, IndividualTree
 
 from django.contrib import admin
 admin.autodiscover()
@@ -12,7 +12,7 @@ provider_urls = patterns('',
     url(r'^test/', TestProviderList.as_view(), name='test-provider-list')
 )
 
-data_urls = patterns('',
+diagnosis_urls = patterns('',
     url(r'^(?P<provider>[a-z]+)/live$', LiveTreeList.as_view(), name='live-tree-list'),
     url(r'^(?P<provider>[a-z]+)/test$', TestTreeList.as_view(), name='test-tree-list'),
     url(r'^tree/' \
@@ -24,7 +24,7 @@ data_urls = patterns('',
 
 urlpatterns = patterns('',
     url(r'^providers/', include(provider_urls)),
-    url(r'^data/', include(data_urls)),
+    url(r'^data/', include(diagnosis_urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^auth', include('rest_framework.urls', namespace='rest_framework'))
 )
