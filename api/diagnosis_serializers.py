3aed36b55be1176029ec95cb0c97feed61586b9c
Sam Whitehall <me@samwhitehall.com>
2013-11-21 16:03:13 +0000

    removed old diagnosis list files

--- api/diagnosis_serializers.py
+++
@@ -1,7 +0,0 @@
-from api.models import Diagnosis
-from rest_framework import serializers
-
-class LiveDiagnosisSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Diagnosis
-        fields = ('name', 'category')
