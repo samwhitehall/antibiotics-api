4eb755bdde9046bc1c1f7b02220965f0c91d784f
Sam Whitehall <me@samwhitehall.com>
2013-11-23 17:15:32 +0000

    structure and version represented in DecisionTree's JSON output

--- api/tree_serializers.py
+++ api/tree_serializers.py
@@ -2,6 +2,9 @@ from api.models import DecisionTree
 from rest_framework import serializers
 
 class IndividualTreeSerializer(serializers.ModelSerializer):
+    version = serializers.Field(source='version')
+    structure = serializers.Field(source='decision_structure')
+
     class Meta:
        model = DecisionTree
-       fields = ('created', 'published')
+       fields = ('created', 'published', 'version', 'structure')
