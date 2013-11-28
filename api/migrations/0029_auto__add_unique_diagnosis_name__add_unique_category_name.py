075afc70fad784a48a92d285ac06631713920196
Sam Whitehall <me@samwhitehall.com>
2013-11-28 20:55:09 +0000

    uniqueness constraints added to categories and diagnoses

---
+++ api/migrations/0029_auto__add_unique_diagnosis_name__add_unique_category_name.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding unique constraint on 'Diagnosis', fields ['name']
+        db.create_unique(u'api_diagnosis', ['name'])
+
+        # Adding unique constraint on 'Category', fields ['name']
+        db.create_unique(u'api_category', ['name'])
+
+
+    def backwards(self, orm):
+        # Removing unique constraint on 'Category', fields ['name']
+        db.delete_unique(u'api_category', ['name'])
+
+        # Removing unique constraint on 'Diagnosis', fields ['name']
+        db.delete_unique(u'api_diagnosis', ['name'])
+
+
+    models = {
+        u'api.category': {
+            'Meta': {'object_name': 'Category'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'})
+        },
+        u'api.decisiontree': {
+            'Meta': {'object_name': 'DecisionTree'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'unique': 'True', 'blank': 'True'}),
+            'decision_structure': ('jsonfield.fields.JSONField', [], {'blank': 'True'}),
+            'diagnosis': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['api.Diagnosis']", 'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'provider': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['api.Provider']", 'null': 'True'}),
+            'published': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        u'api.diagnosis': {
+            'Meta': {'object_name': 'Diagnosis'},
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['api.Category']", 'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'})
+        },
+        u'api.provider': {
+            'Meta': {'object_name': 'Provider'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'})
+        },
+        u'api.question': {
+            'Meta': {'object_name': 'Question'},
+            'answers': ('jsonfield.fields.JSONField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'information': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'label': ('django.db.models.fields.CharField', [], {'max_length': '100', 'blank': 'True'}),
+            'qid': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'api.treatment': {
+            'Meta': {'object_name': 'Treatment'},
+            'details': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'tid': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        }
+    }
+
+    complete_apps = ['api']
--- api/models.py
+++ api/models.py
@@ -18,7 +18,7 @@ class Provider(models.Model):
 
 class Category(models.Model):
     slug = models.SlugField(unique=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(unique=True, max_length=100)
 
     def __unicode__(self):
         return self.name
@@ -28,7 +28,7 @@ class Category(models.Model):
 
 class Diagnosis(models.Model):
     slug = models.SlugField(unique=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(unique=True, max_length=100)
     category = models.ForeignKey(Category, null=True)
 
     def __unicode__(self):
