574df4bee668fd99545433ebe9d1b22c879a3c1d
Sam Whitehall <me@samwhitehall.com>
2013-11-28 22:37:00 +0000

    6/8 (non-dynamic) tests implemented for ProviderListViewTest

--- api/tests.py
+++ api/tests.py
@@ -1,5 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from api.models import Provider, Category, Diagnosis, DecisionTree
+
+import simplejson as json
 
 class DecisionTreeVersionTest(TestCase):
     fixtures = ['unittest.json']
@@ -83,19 +85,55 @@ class ProviderListViewTest(TestCase):
     fixtures = ['providers.json', 'categories.json', 'diagnoses.json', 
         'questions.json', 'treatments.json', 'trees.json']
 
+    def live_setUp(self):
+        response = Client().get('/providers/')
+        self.assertEqual(response.status_code, 200)
+        self.content = json.loads(response.content)
+
+    def test_setUp(self):
+        response = Client().get('/providers/test')
+        self.assertEqual(response.status_code, 200)
+        self.content = json.loads(response.content)
+
     def test_live_contains_suht(self):
-        self.assertTrue(False)
+        self.live_setUp()
+        self.assertEqual(self.content[0]['slug'], 'suht')
 
     def test_live_contains_correct_fields(self):
-        self.assertTrue(False)
+        self.live_setUp()
+        self.assertIn('name', self.content[0].keys())
+        self.assertIn('description', self.content[0].keys())
+        self.assertEquals(self.content[0]['status'], 'live')
 
     def test_test_contains_suht_phnt(self):
-        self.assertTrue(False)
+        self.test_setUp()
+        self.assertEqual(self.content[0]['slug'], 'suht')
+        self.assertEqual(self.content[1]['slug'], 'phnt')
 
     def test_test_contains_correct_fields(self):
-        self.assertTrue(False)
+        self.test_setUp()
+
+        self.assertSetEqual(set(self.content[0].keys()), 
+            {'slug', 'name', 'description', 'status'})
+        self.assertSetEqual(set(self.content[1].keys()), 
+            {'slug', 'name', 'description', 'status'})
+
+        self.assertEqual(self.content[0]['status'], 'test')
+        self.assertEqual(self.content[1]['status'], 'test')
 
     def test_default_url_is_live_url(self):
+        self.live_setUp()
+
+        live = Client().get('/providers/live')
+        self.assertEqual(live.status_code, 200)
+
+        content = json.loads(live.content)
+        self.assertEqual(content, self.content)
+
+    def test_add_provider_no_published(self):
+        self.assertTrue(False)
+
+    def test_add_provider_published(self):
         self.assertTrue(False)
 
 class DiagnosisListViewTest(TestCase):
@@ -117,6 +155,15 @@ class DiagnosisListViewTest(TestCase):
     def test_default_url_is_live_url(self):
         self.assertTrue(False)
 
+    def test_add_new_diagnosis_no_implementation(self):
+        self.assertTrue(False)
+
+    def test_add_new_unpublished_diagnosis_implementation(self):
+        self.assertTrue(False)
+
+    def test_add_new_published_diagnosis_implementation(self):
+        self.assertTrue(False)
+
 class IndividualTreeViewTest(TestCase):
     fixtures = ['providers.json', 'categories.json', 'diagnoses.json', 
         'questions.json', 'treatments.json', 'trees.json']
@@ -134,4 +181,10 @@ class IndividualTreeViewTest(TestCase):
         self.assertTrue(False)
 
     def test_conforms_to_schema(self):
+        self.assertTrue(False)
+
+    def test_add_remove_question_to_tree(self):
+        self.assertTrue(False)
+
+    def test_add_remove_treatment_to_tree(self):
         self.assertTrue(False)
