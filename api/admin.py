a852a1bf59575ed58c6952dd895e0d8779fb7057
Sam Whitehall <me@samwhitehall.com>
2013-12-24 13:57:03 +0000

    admin interface tweaks: can't edit once created, must make new one. version is shown for DT on its admin page. also list display for decision trees

--- api/admin.py
+++ api/admin.py
@@ -5,7 +5,19 @@ from api.models import Provider, Category, Diagnosis, DecisionTree, \
 admin.site.register(Provider)
 admin.site.register(Category)
 admin.site.register(Diagnosis)
-admin.site.register(DecisionTree)
 
 admin.site.register(Question)
 admin.site.register(Treatment)
+
+class DecisionTreeAdmin(admin.ModelAdmin):
+    model = DecisionTree
+    view_on_site = True
+    list_display = ('diagnosis', 'provider', 'version',)
+    fields = ('published', 'version', 'provider', 'diagnosis', 'decision_structure')
+
+    def get_readonly_fields(self, req, obj=None):
+        if obj: # in edit mode
+            return ('created', 'provider', 'diagnosis', 'version', 'decision_structure')
+        return ('version', 'created')
+
+admin.site.register(DecisionTree, DecisionTreeAdmin)
